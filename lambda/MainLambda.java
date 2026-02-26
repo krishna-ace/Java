1e5fff5e64ba95ce357d8a3e1976092863f04560
krishna-ace <kp75617@gmail.com>
2026-02-26 10:03:22 +0530

    Refactor MainLambda to use MyInterface with lambda

--- lambda/MainLambda.java
+++ lambda/MainLambda.java
@@ -1,6 +1,27 @@
 package lambda;
 @FunctionalInterface
-interface calc{
-        void add (int a,int b );
+interface MyInterface {
+
+    // abstract method
+    String reverse(String n);
+}
+
+public class Main {
+
+    public static void main( String[] args ) {
+
+        // declare a reference to MyInterface
+        // assign a lambda expression to the reference
+        MyInterface ref = (str) -> {
+
+            String result = "";
+            for (int i = str.length()-1; i >= 0 ; i--)
+            result += str.charAt(i);
+            return result;
+        };
+
+        // call the method of the interface
+        System.out.println("Lambda reversed = " + ref.reverse("Lambda"));
     }
-    public class MainLambda
+
+}
