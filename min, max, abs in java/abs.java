17c727d6b93e0aa2ad04326123a7caf762287f32
krishna-ace <kp75617@gmail.com>
2026-04-13 22:06:48 +0530

    Add files via upload

---
+++ min, max, abs in java/abs.java
@@ -0,0 +1,8 @@
+public class AbsExample {
+    public static void main(String[] args) {
+        int a = -15;
+
+        int result = Math.abs(a);
+        System.out.println("Absolute value: " + result);
+    }
+}
