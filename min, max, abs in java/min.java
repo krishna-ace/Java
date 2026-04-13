6255b6483d1e8971a86fff1a3d405734ab94b872
krishna-ace <kp75617@gmail.com>
2026-04-13 22:07:42 +0530

    Add files via upload

---
+++ min, max, abs in java/min.java
@@ -0,0 +1,9 @@
+public class MinExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int result = Math.min(a, b);
+        System.out.println("Minimum value: " + result);
+    }
+}
