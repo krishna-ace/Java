766ca816f948a2e0d2040f669175c48d4aa154b6
krishna-ace <kp75617@gmail.com>
2026-04-13 22:04:58 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,15 @@
+public class AllFunctionsExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = -15;
+
+        int minValue = Math.min(a, b);
+        int maxValue = Math.max(a, b);
+        int absValue = Math.abs(c);
+
+        System.out.println("Minimum value: " + minValue);
+        System.out.println("Maximum value: " + maxValue);
+        System.out.println("Absolute value: " + absValue);
+    }
+}
