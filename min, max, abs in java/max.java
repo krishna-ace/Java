d85a2f78c81930f1d10515afb5a4fb070bae51c9
krishna-ace <kp75617@gmail.com>
2026-04-13 22:10:34 +0530

    Add files via upload

---
+++ min, max, abs in java/max.java
@@ -0,0 +1,9 @@
+public class MaxExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int result = Math.max(a, b);
+        System.out.println("Maximum value: " + result);
+    }
+}
