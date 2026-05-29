144eb1af3f449ea599c5b5e7b66183c7370bcd51
krishna-ace <kp75617@gmail.com>
2026-05-29 23:11:11 +0530

    Add pow class to calculate power of a number

---
+++ min, max, abs in java/pow.java
@@ -0,0 +1,9 @@
+public class pow {
+    public static void main(String[] args) {
+
+        int base = 2;
+        int power = 5;
+
+        System.out.println("Power: " + Math.pow(base, power));
+    }
+}
