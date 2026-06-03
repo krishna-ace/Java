c26d60df6497d1cff9f912b23ee8e4b0c65d19e6
krishna-ace <kp75617@gmail.com>
2026-06-03 21:34:24 +0530

    Implement Anagram Checker using HashMap

---
+++ Hashmap/Anagram Checker using HashMap.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Anagram {
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s2.toCharArray()) {
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+
+            if (map.get(c) == 0)
+                map.remove(c);
+        }
+
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+    }
+}
